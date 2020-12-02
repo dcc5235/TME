675e966becd8ac4f6abff6b9bb56650f4e5b85c4
Dany <dany@Danys-MacBook-Pro.local>
2020-12-02 11:51:33 -0600

    refactor

--- sampleproject/test/forEach.test.js
+++ sampleproject/test/forEach.test.js
@@ -2,12 +2,12 @@ const assert = require('assert');
 const { forEach } = require('../index');
 
 it('should sum an array', () => {
-  const numbers = [1,2,3];
+  const numbers = [1, 2, 3];
 
   let total = 0;
-  forEach(numbers, (value) => {
+  forEach(numbers, value => {
     total += value;
   });
 
   assert.strictEqual(total, 6);
-})
+});
